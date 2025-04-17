af56a6359a10d97c4917b9376b39ee8ac7f9832a
Pavel Lishin <pavel.lishin@yandex.ru>
2025-04-17 12:32:53 +0300

    test1

--- db.go
+++ db.go
@@ -1,47 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
-
 func InitDB() {
 	err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file:", err)
-    }
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-        os.Getenv("DB_HOST"),
-        os.Getenv("DB_USER"),
-        os.Getenv("DB_PASSWORD"),
-        os.Getenv("DB_NAME"),
-        os.Getenv("DB_PORT"),
-    )
-	
-	var errDB error
-    db, errDB = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if errDB != nil {
-        log.Fatal("Error connecting to database:", errDB)
-    }
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
-    log.Println("Successfully connected to database!")
+	var errDB error
+	db, errDB = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if errDB != nil {
+		log.Fatal("Error connecting to database:", errDB)
+	}
+
+	log.Println("Successfully connected to database!")
 }
 
 func MigrateDB() {
-    // Автоматическое создание таблиц
-    err := db.AutoMigrate(&User{}, &Genre{}, &Status{}, &Book{})
-    if err != nil {
-        log.Fatal("Error migrating database:", err)
-    }
+	// Автоматическое создание таблиц
+	err := db.AutoMigrate(&User{}, &Genre{}, &Status{}, &Book{})
+	if err != nil {
+		log.Fatal("Error migrating database:", err)
+	}
 
-    log.Println("Database tables created/updated successfully!")
+	log.Println("Database tables created/updated successfully!")
 }
